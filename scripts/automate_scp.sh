026ef3c40d5d106539eceb4e996beea89eaae55d
DylanHR <hrdylan@gmail.com>
2019-04-23 02:17:22 -0400

    scripts

---
+++ scripts/automate_scp.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+$PATH = /Users/DylanHR/desktop/dis-sys
+./scp_apache.sh 3.94.170.64
+./scp_apache.sh 100.26.104.102
+./scp_apache.sh 3.83.10.22
+./scp_apache.sh 3.14.64.40
+./scp_apache.sh 3.14.67.164
+./scp_apache.sh 54.153.0.234
+./scp_apache.sh 54.193.125.205
+./scp_apache.sh 52.24.143.203
+./scp_apache.sh 34.221.171.231
+./scp_apache.sh 35.183.5.27
+./scp_apache.sh 35.182.195.196
+./scp_apache.sh 13.233.35.167
+./scp_apache.sh 13.233.83.84
+./scp_apache.sh 13.229.131.254
+./scp_apache.sh 3.0.94.77
+./scp_apache.sh 54.252.169.133
+./scp_apache.sh 13.54.28.88
+./scp_apache.sh 54.250.199.120
+./scp_apache.sh 13.112.35.80
+./scp_apache.sh 18.185.138.61
+./scp_apache.sh 52.59.249.174
+./scp_apache.sh 34.249.192.207
+./scp_apache.sh 54.229.234.118
+./scp_apache.sh 35.181.59.157
+./scp_apache.sh 35.181.43.210
+./scp_apache.sh 18.130.245.254
+./scp_apache.sh 35.178.15.57
+./scp_apache.sh 54.233.223.52
+./scp_apache.sh 18.228.190.7
